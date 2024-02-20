decc449581c73c5b5d0a3a86a10b42e5ad511b9f
diana esteves <diana@pulumi.com>
2024-02-20 16:04:55 -0600

    solution WIP

--- module-09-policy-as-code/exercises/infra/index.ts
+++ module-09-policy-as-code/exercises/infra/index.ts
@@ -1,6 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as docker from "@pulumi/docker";
 
 const config = new pulumi.Config();
 
@@ -14,13 +13,4 @@ new aws.ec2.Instance("policy-as-code-workshop-"+name, {
   ami: "ami-08f7912c15ca96832", // Replace with the AMI ID for your region, us-west-2
   associatePublicIpAddress: false,
   instanceType: "t3.micro"
-});
-
-new docker.Image("policy-as-code-workshop-"+name, {
-  imageName: "docker.io/joshkodroff/pulumi-policy-test",
-  buildOnPreview: false,
-  build: {
-    dockerfile: "../app/Dockerfile",
-    platform: "linux/amd64"
-  }
 });
