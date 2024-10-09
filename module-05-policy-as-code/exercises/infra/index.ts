5aeaaa87826e39d0080c56393357d18374db2097
Josh Kodroff <josh@pulumi.com>
2024-10-09 09:50:16 -0400

    Fix up PaC exercise.

--- module-05-policy-as-code/exercises/infra/index.ts
+++ module-05-policy-as-code/exercises/infra/index.ts
@@ -3,12 +3,9 @@ import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config();
 
-// pulumi config set MY_NAME bugs-bunny
-const name = config.require("MY_NAME");
+new aws.s3.Bucket("policy-as-code-workshop-");
 
-new aws.s3.Bucket("policy-as-code-workshop-" + name);
-
-new aws.ec2.Instance("policy-as-code-workshop-" + name, {
+new aws.ec2.Instance("policy-as-code-workshop-", {
   // https://cloud-images.ubuntu.com/locator/ec2/
   ami: "ami-08f7912c15ca96832", // Replace with the AMI ID for your region, us-west-2
   associatePublicIpAddress: true,
